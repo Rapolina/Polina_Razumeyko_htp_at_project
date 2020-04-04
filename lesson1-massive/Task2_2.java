6a439eedd8cb4abb3283b21e50a88b2c34218157
PRazumeiko <polina.razumeiko@yandex.ru>
2020-04-04 11:36:04 +0300

    release v1

---
+++ lesson1-massive/Task2_2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 26) {
+            System.out.print(a + " ");
+            a++;
+        }
+    }
+}
